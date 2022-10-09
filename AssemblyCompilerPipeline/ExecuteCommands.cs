90542d010973823099a9a7f903538d10e92d766b
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2022-10-09 02:21:51 -0400

    Basic compilation, linking, and execution

---
+++ AssemblyCompilerPipeline/ExecuteCommands.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace AssemblyCompilerPipeline;
+
+public static class ExecuteCommands
+{
+    public static string ExecuteCommand(string cmd)
+
+    {
+        try
+        {
+            using var compiler = new Process();
+            
+            compiler.StartInfo.FileName = "cmd.exe";
+            compiler.StartInfo.Arguments = "/C " + cmd;
+            compiler.StartInfo.UseShellExecute = false;
+            compiler.StartInfo.RedirectStandardOutput = true;
+            compiler.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            compiler.Start();
+
+            var output = compiler.StandardOutput.ReadToEnd();
+
+            compiler.WaitForExit();
+           
+            return output.Replace("\n", "").Replace("\r", "").Trim();
+        }
+
+        catch (Exception err)
+        {
+            return $"Error running command : {err.Message}\n";
+        }
+    }
+}
---
+++ AssemblyCompilerPipeline/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace AssemblyCompilerPipeline;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        if(args.Length < 3)
+            return;
+        
+        var nasmPath = args[0];
+        var gccPath = args[1];
+        var asmFile = args[2];
+
+        Console.WriteLine("Compiling to Object file...");
+        CompileToObject(nasmPath, asmFile, out var compileResult);
+        Console.WriteLine("CompileResult: " + compileResult);
+        Console.WriteLine("Linking to Executable file...");
+        Link(gccPath, asmFile, out var linkResult);
+        Console.WriteLine("LinkResult: " + linkResult);
+        Console.WriteLine("Executing...");
+        Execute(asmFile, out var output);
+        Console.WriteLine("Output: " + output);
+    }
+
+    private static void CompileToObject(string nasm, string file, out string result)
+    {
+        string text = nasm + " -fwin32 " + file;
+        result = ExecuteCommands.ExecuteCommand(text);
+    }
+    
+    private static void Link(string gcc, string file, out string result)
+    {
+        string rawFilePath = file.Replace(".asm", "");
+        string text = gcc  + " " + rawFilePath + ".obj" + " -o " + rawFilePath + ".exe"; 
+        result = ExecuteCommands.ExecuteCommand(text);
+    }
+    
+    private static void Execute(string file, out string result)
+    {
+        string text = file.Replace(".asm", ".exe");
+        result = ExecuteCommands.ExecuteCommand(text);
+    }
+}
